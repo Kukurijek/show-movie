6c48bd58f0f626bdb7462f3b3bc2aedbe3effd03
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-03-06 12:02:46 +0100

    add default values to data calss

--- app/src/main/java/com/nemanja/filipovic/showmovie/Movie.kt
+++ app/src/main/java/com/nemanja/filipovic/showmovie/Movie.kt
@@ -1,10 +1,10 @@
 package com.nemanja.filipovic.showmovie
 
 data class Movie (
-    var title: String,
-    var description: String,
-    var rating: Float,
+    var title: String = "Catch me if you can",
+    var description: String = "Drama",
+    var rating: Float = 3.7182818284f,
     var genres: String?,
-    var creatorsAuthors: String,
-    var actors: String
+    var creatorsAuthors: String = "Steven Spielberg;",
+    var actors: String = "Leonardo DiCaprio"
 )
