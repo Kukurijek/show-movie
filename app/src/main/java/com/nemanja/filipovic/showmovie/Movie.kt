43d3b0bc42d65f9e881908afea27185ea6033d67
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-03-06 14:21:32 +0100

    give default vaule null to genres

--- app/src/main/java/com/nemanja/filipovic/showmovie/Movie.kt
+++ app/src/main/java/com/nemanja/filipovic/showmovie/Movie.kt
@@ -4,7 +4,7 @@ data class Movie (
     var title: String = "Catch me if you can",
     var description: String = "Drama",
     var rating: Float = 3.7182818284f,
-    var genres: String?,
+    var genres: String? = null,
     var creatorsAuthors: String = "Steven Spielberg;",
     var actors: String = "Leonardo DiCaprio"
 )
