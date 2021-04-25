41b06fe7b9d7eceb44b0de9b7d173c72ba9085fb
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-04-25 23:10:38 +0200

    override lifecycle methods and add answers to the questions

--- app/src/main/java/com/example/mad03_fragments_and_navigation/MainActivity.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.mad03_fragments_and_navigation.databinding.ActivityMainBinding
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -28,6 +29,50 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("MainActivity", "I am in onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("MainActivity", "I am in onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("MainActivity", "I am in onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("MainActivity", "I am in onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("MainActivity", "I am in onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i("MainActivity", "I am in onRestart")
+    }
+
+
+
+    // if you start the app, the method onCreate will be called at beginning, then onStart then onResume.
+    // If you press backButton the method onDestroy is called
+    // If you switch to landscape mode, the method onConfigurationChanged will be called  and from the default methods - onStop - onCreate and then onStart
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, drawerLayout)
